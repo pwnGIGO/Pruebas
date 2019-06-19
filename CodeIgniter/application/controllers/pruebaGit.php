0219644f2d3494ebc6816087d79f642b1be566ed
josse2809 <joseuam_cbi@hotmail.com>
2019-06-19 12:30:25 -0500

    cambios jose... , creo 12:30.. esque me creo unas cosas en sublimtext, ya las borre

--- CodeIgniter/application/controllers/pruebaGit.php
+++ CodeIgniter/application/controllers/pruebaGit.php
@@ -5,11 +5,7 @@
 	}
 	function Francisca(){}
 
-<<<<<<< HEAD
         function jose(){
         	$variable_jose="holaa";
 
         }
-=======
-	function jose(){}
->>>>>>> b2b59fcf374eff9b8c49d7388808fd24b71b9ee9
