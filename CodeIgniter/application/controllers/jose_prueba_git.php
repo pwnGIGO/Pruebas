16fd547afc1493ea6d4c948cdc0e59cf805a24ef
unknown <gigino_44_@hotmail.com>
2019-06-21 14:31:15 -0500

    Agregue mas lineas a jose_prueba_git

--- CodeIgniter/application/controllers/jose_prueba_git.php
+++ CodeIgniter/application/controllers/jose_prueba_git.php
@@ -1,6 +1,10 @@
 <?php 
 	public function hola(){
 		echo "ya cambie en mi propio controlador";
+		echo "ya cambie en mi propio controlador";
+		echo "ya cambie en mi propio controlador";
+		echo "ya cambie en mi propio controlador";
+		echo "ya cambie en mi propio controlador";
 	}
 
 
